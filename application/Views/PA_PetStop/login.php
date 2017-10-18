87521c9d825053a78d099d2977d908df2f43baad
Marcos de Andrade Pinto Filho <marcosandradeapf@gmail.com>
2017-10-18 02:08:26 -0200

    Adicionando arquivos base da View
    Header, parte do corpo da página inicial e página de login

---
+++ application/Views/PA_PetStop/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Teste</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> <!--Bootstrap 4 CSS Library-->
+	<link rel="stylesheet" href="login.css"> <!--Local Javascript-->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> <!--Jquery Import-->
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script> <!--Bootstrap 4 Import-->
+	<script type="text/javascript" src="https://use.fontawesome.com/574c45c449.js"></script>
+	<script type="text/javascript" src="login.js"></script> <!--Local Javascript-->
+</head>
+<body>	
+	<div id="background-image"></div>
+	<main class="login-pane">
+		<figure class="store-logo"></figure>
+		<form action="" method="post">
+			<div class="clearfix">
+				<div class="form-group"><input class="form-control" type="text" placeholder="Username"></div>
+				<div class="form-group"><input class="form-control" type="text" placeholder="Password"></div>
+				<div class="form-group pull-left"><button type="button" class="btn btn-danger">Entrar</button></div>
+				<div class="form-group pull-right"><label class="checkbox"><input type="checkbox" value="remember-me"> Lembrar Senha</label></div>
+			</div>
+			<div class="password-alert"><a href="#">Recuperar senha perdida</a></div>
+		</form>
+	<main>
+</body>
+</html>
