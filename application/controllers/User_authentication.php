c934bba18f4ca2c50ca6ef181bc6fd40aa3853e9
Fernando Mendes <fernando_moreiram@hotmail.com>
2017-11-15 21:34:46 -0200

    Configuracoes de url do heroku

--- application/controllers/User_authentication.php
+++ application/controllers/User_authentication.php
@@ -50,7 +50,7 @@ class User_Authentication extends CI_Controller
             $data['authUrl'] =  $this->facebook->login_url();
         }
         // Load login & profile view
-        $this->load->view('login/login',$data);
+        $this->load->view('teste2',$data);
     }
 
     public function logout() {
