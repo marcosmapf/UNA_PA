ad5931e9d235932496e6e2a9ef479fef35a3fe66
Marcos de Andrade Pinto Filho <marcosandradeapf@gmail.com>
2018-04-08 14:07:02 -0300

    Adicionando selo de segurança para verificar vulnerabilidades do site

--- application/views/main/main.php
+++ application/views/main/main.php
@@ -404,7 +404,9 @@
     <footer id="page-footer-grid" class="d-inline-flex">
       <!--<img src="../../../assets/imagens/logoVermelho3.png" width="2.5%">-->
       <div class="mr-auto p-2">
-        <div></div>
+      <a href="https://seal.beyondsecurity.com/vulnerability-scanner-verification/petshopecommerce.herokuapp.com">
+        <img src="https://seal.beyondsecurity.com/verification-images/petshopecommerce.herokuapp.com/vulnerability-scanner-2.gif" alt="Website Security Test" border="0" />
+      </a>
       </div>
       <div class="mx-auto p-2">
         <div><img id="logo-reduzida" src="<?=base_url()?>/assets/imagens/logo-pequena-borda.png"> Pet Stop</div>
