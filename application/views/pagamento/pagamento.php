ecc0837c23ed8c6db04721f2ef0060bfc81b0e4f
Marcos de Andrade Pinto Filho <marcosandradeapf@gmail.com>
2018-04-09 18:37:36 -0300

    Estrutura base da view de pagamentos (grid-layout)
    O HTML e CSS foram colocados juntos para facilitar o desenvolvimento, sendo que o CSS e o Javascript devem ser transferidos para a pasta de assets futuramente

---
+++ application/views/pagamento/pagamento.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="pagamento.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> <!--Jquery Import-->
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script> <!--Bootstrap 4 Import-->
+    <script type="text/javascript" src="https://use.fontawesome.com/574c45c449.js"></script> <!--Font Awesome Import-->
+    <title>Pet Stop</title>	
+</head>
+<body>
+    <div id="wrapper"></div>
+        <header id="grid_header">
+            <div id="grid_header-logo"></div>
+            <nav id="grid_header-navbar"></nav>
+            <div id="grid_header-icons"></div>
+        </header>
+        <section id="grid_confirmacao-compra">
+            <div id="grid_confirmacao-valores"></div>
+            <div id="grid_confirmacao-entrega"></div>
+            <div id="grid_revisao-produtos"></div>
+        </section>
+        <section id="grid_pagamento">
+            <div id="grid_forma-pagamento"></div>
+            <div id="grid_dados-pagamento"></div>
+        </section>
+        <footer id="grid_footer">
+            <div id="grid_footer-logo"></div>
+            <div id="grid_footer-social-media"></div>
+        </footer>
+    </div>
+</body>
