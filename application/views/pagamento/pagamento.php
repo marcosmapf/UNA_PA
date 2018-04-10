eabec656f2fca87b8632f834cc8be46e22b3aa7b
Marcos de Andrade Pinto Filho <marcosandradeapf@gmail.com>
2018-04-10 19:23:00 -0300

    CSS e HTML de forma de pagamento, endereco entrega e fix pequenos no layout geral

--- application/views/pagamento/pagamento.php
+++ application/views/pagamento/pagamento.php
@@ -9,27 +9,55 @@
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> <!--Jquery Import-->
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script> <!--Bootstrap 4 Import-->
     <script type="text/javascript" src="https://use.fontawesome.com/574c45c449.js"></script> <!--Font Awesome Import-->
-    <title>Pet Stop</title>	
+    <title>Pet Stop</title>
 </head>
 <body>
-    <div id="wrapper"></div>
-        <header id="grid_header">
-            <div id="grid_header-logo"></div>
-            <nav id="grid_header-navbar"></nav>
-            <div id="grid_header-icons"></div>
-        </header>
+    <header id="grid_header">
+        <div id="grid_header-logo">logo</div>
+        <nav id="grid_header-navbar">navbar</nav>
+        <div id="grid_header-icons">icones</div>
+    </header>
+    <div id="wrapper">
         <section id="grid_confirmacao-compra">
-            <div id="grid_confirmacao-valores"></div>
-            <div id="grid_confirmacao-entrega"></div>
-            <div id="grid_revisao-produtos"></div>
+            <div id="grid_metodo-entrega">
+                <div id="grid_opcoes-entrega">                    
+                    <div class="card" id="pagamento-pac">
+                        <img src="pac.png" alt="PAC">    
+                        <div class="conteudo">
+                            <input type="radio">            
+                        </div>
+                    </div>
+                    <div class="card" id="pagamento-sedex">
+                        <img src="sedex.png" alt="Sedex">
+                        <div class="conteudo">    
+                            <input type="radio">
+                        </div>           
+                    </div>
+                </div>
+                <div id="grid_valor-entrega">
+                    <p>Frete para {{endereco-cidade}}: R$ {{valor}}</p>
+                </div>
+            </div>
+            <div id="grid_info-entrega">
+                <h3> Endereço de Entrega </h3>
+                <p id="nome-cliente">{{nome-cliente}}</p>
+                <p id="endereco-residencia">{{endereco-residencia}}</p>
+                <p id="endereco-bairro">{{endereco-bairro}}</p>
+                <p id="endereco-cidade">{{endereco-cidade}} - {{endereco-UF}}</p>
+                <p id="endereco-cep">{{endereco-cep}}</p>
+                <p id="alterar-endereco">Alterar endereço de entrega</p>
+            </div>
+            <div id="grid_revisao-produtos">
+                <p>revisao produtos</p>
+            </div>
         </section>
         <section id="grid_pagamento">
-            <div id="grid_forma-pagamento"></div>
-            <div id="grid_dados-pagamento"></div>
+            <div id="grid_metodo-pagamento">metodo pagamento</div>
+            <div id="grid_dados-pagamento">informacao pagamento</div>
         </section>
-        <footer id="grid_footer">
-            <div id="grid_footer-logo"></div>
-            <div id="grid_footer-social-media"></div>
-        </footer>
     </div>
+    <footer id="grid_footer">
+        <div id="grid_footer-logo">logo</div>
+        <div id="grid_footer-social-media">midias sociais</div>
+    </footer>
 </body>
