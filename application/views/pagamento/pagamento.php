f8a46ed4cdd3699403d414e47c1aabf17f5f940a
Marcos de Andrade Pinto Filho <marcosandradeapf@gmail.com>
2018-04-14 12:47:57 -0300

    Finalizando seção de dados do pagamento
    Adicionando imagens de exemplo de design para utilizar no futuro

--- application/views/pagamento/pagamento.php
+++ application/views/pagamento/pagamento.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <link rel="stylesheet" href="pagamento.css">
+    <script type="text/javascript" src="pagamento.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> <!--Jquery Import-->
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script> <!--Bootstrap 4 Import-->
     <script type="text/javascript" src="https://use.fontawesome.com/574c45c449.js"></script> <!--Font Awesome Import-->
@@ -24,13 +25,13 @@
                     <div class="card">
                         <img src="pac.png" alt="PAC">    
                         <div class="conteudo">
-                            <input type="radio">            
+                            <input id="pac-radio" type="radio" checked="checked">            
                         </div>
                     </div>
                     <div class="card">
                         <img src="sedex.png" alt="Sedex">
                         <div class="conteudo">    
-                            <input type="radio">
+                            <input id="sedex-radio" type="radio">
                         </div>           
                     </div>
                 </div>
@@ -45,7 +46,7 @@
                 <p id="endereco-bairro">{{endereco-bairro}}</p>
                 <p id="endereco-cidade">{{endereco-cidade}} - {{endereco-UF}}</p>
                 <p id="endereco-cep">{{endereco-cep}}</p>
-                <p id="alterar-endereco">Alterar endereço de entrega</p>
+                <a href="#"><p id="alterar-endereco">Alterar endereço de entrega</p></a>
             </div>
             <div id="grid_revisao-produtos">
                 <p>revisao produtos</p>
@@ -57,6 +58,27 @@
             </div>
             <div id="grid_dados-pagamento">
                 <!--Se o pagamento for através de cartão de crédito-->
+                <div id="grid_pagamento-boleto">
+                    <div>
+                        <svg viewBox="0 0 64 64">
+                            <path fill="#666" d="M 32 0 A 32 32 0 0 0 0 32 A 32 32 0 0 0 32 64 A 32 32 0 0 0 64 32 A 32 32 0 0 0 32 0 z M 20 14 L 44 14 L 44 21 L 20 21 L 20 14 z M 16.5 23 L 47.5 23 C 49.4 23 51 24.6 51 26.5 L 51 41.5 C 51 43.4 49.4 45 47.5 45 L 46 45 L 46 30 L 18 30 L 18 45 L 16.5 45 C 14.6 45 13 43.4 13 41.5 L 13 26.5 C 13 24.6 14.6 23 16.5 23 z M 21 33 L 43 33 L 43 52 L 21 52 L 21 33 z M 23 36 L 23 37 L 41 37 L 41 36 L 23 36 z M 23 39 L 23 40 L 41 40 L 41 39 L 23 39 z M 23 42 L 23 43 L 41 43 L 41 42 L 23 42 z M 23 45 L 23 46 L 41 46 L 41 45 L 23 45 z M 23 48 L 23 49 L 41 49 L 41 48 L 23 48 z "></path>
+                        </svg>
+                        <span>Imprima o boleto e pague no banco</span>
+                    </div>
+                    <div>
+                        <svg viewBox="0 0 64 64">
+                            <path fill="#666" d="M 32 0 A 32 32 0 0 0 0 32 A 32 32 0 0 0 32 64 A 32 32 0 0 0 64 32 A 32 32 0 0 0 32 0 z M 15.7 16 L 49.3 16 C 50.8 16 52 17.2 52 18.7 L 52 20 L 49 20 L 49 19.9 C 49 19.4 48.6 19 48.1 19 L 16.9 19 C 16.4 19 16 19.4 16 19.9 L 16 38.1 C 16 38.6 16.4 39 16.9 39 L 48.1 39 C 48.6 39 49 38.6 49 38.1 L 49 37 L 52 37 L 52 39.3 C 52 40.8 50.8 42 49.3 42 L 15.7 42 C 14.2 42 13 40.8 13 39.3 L 13 18.7 C 13 17.2 14.2 16 15.7 16 z M 36 22 L 57 22 L 57 35 L 36 35 L 36 22 z M 39 24 L 39 33 L 41 33 L 41 24 L 39 24 z M 43 24 L 43 33 L 44 33 L 44 24 L 43 24 z M 45 24 L 45 33 L 46 33 L 46 24 L 45 24 z M 48 24 L 48 33 L 50 33 L 50 24 L 48 24 z M 52 24 L 52 33 L 53 33 L 53 24 L 52 24 z M 54 24 L 54 33 L 55 33 L 55 24 L 54 24 z M 10 44 L 28 44 C 28 44.6 28.4 45 29 45 L 37 45 C 37.5 45 38 44.6 38 44 L 54 44 C 54.6 44 54.8 44.4 54.5 44.9 L 53.4 47.1 C 53.2 47.6 52.5 48 52 48 L 12 48 C 11.5 48 10.8 47.6 10.6 47.1 L 9.5 44.9 C 9.2 44.4 9.5 44 10 44 z M 50 45 C 49.4 45 49 45.4 49 46 C 49 46.6 49.4 47 50 47 C 50.6 47 51 46.6 51 46 C 51 45.4 50.6 45 50 45 z "></path>
+                        </svg>
+                        <span>ou pague pela internet utilizando o código de barras do boleto</span>
+                    </div>
+                    <div>
+                        <svg viewBox="0 0 64 64">
+                            <path fill="#666" d="M 32 0 C 14.3 0 0 14.3 0 32 C 0 49.7 14.3 64 32 64 C 49.7 64 64 49.7 64 32 C 64 14.3 49.7 0 32 0 z M 24 9 C 25.1 9 26 9.9 26 11 L 26 18 C 26 19.1 25.1 20 24 20 C 22.9 20 22 19.1 22 18 L 22 11 C 22 9.9 22.9 9 24 9 z M 40 9 C 41.1 9 42 9.9 42 11 L 42 18 C 42 19.1 41.1 20 40 20 C 38.9 20 38 19.1 38 18 L 38 11 C 38 9.9 38.9 9 40 9 z M 14 15 L 20 15 L 20 18.4 C 20 20.4 21.6 22 23.6 22 L 24.4 22 C 26.4 22 28 20.4 28 18.4 L 28 15 L 36 15 L 36 18.4 C 36 20.4 37.6 22 39.6 22 L 40.4 22 C 42.4 22 44 20.4 44 18.4 L 44 15 L 50 15 C 50.6 15 51 15.5 51 16 L 51 48 C 51 48.6 50.5 49 50 49 L 14 49 C 13.4 49 13 48.5 13 48 L 13 16 C 13 15.4 13.5 15 14 15 z M 16 24.3 L 16 45.3 L 48 45.3 L 48 24.3 L 16 24.3 z "></path>
+                        </svg>
+                        <span>O prazo de validade do boleto é de 3 dias úteis</span>
+                    </div>
+                </div>
+
                 <div id="grid_pagamento-cartao">
                     <div id="grid_pagamento-cartao-numero">
                         <label for="cartao-numero">Número do Cartão</label>
@@ -103,10 +125,10 @@
                         <label for="cartao-parcelas-">Número de parcelas</label>
                         <input id="cartao-parcelas" type="text"> <!-- Adicionar pattern e mask com angular válidas com angular ou php-->
                     </div>
-                    <div id="grid_pagamento-cartao-pagar">
-                        <label for="pagamento-pagar-botao">Valor Total: R$ {{valor-total}}</label>
-                        <button id="pagamento-pagar-botao">Pagar com cartão de crédito</button>
-                    </div>
+                </div>
+                <div id="grid_pagamento-pagar">
+                    <label for="pagamento-pagar-botao">Valor Total: R$ {{valor-total}}</label>
+                    <button id="pagamento-pagar-botao">Pagar com cartão de crédito</button>
                 </div>          
             </div>
         </section>
